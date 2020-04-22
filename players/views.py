4264f5fb761068cdde8ed049fd1ab7877706efb2
hank-weber <hpw3nr@virginia.edu>
2020-04-22 18:40:59 -0400

    add backend support of filtering with query params

--- players/views.py
+++ players/views.py
@@ -1,12 +1,14 @@
 from django.db import connection
-from django.views.generic import ListView
 
 from django.shortcuts import get_object_or_404, render
 
+player_fields = ['long_name', 'age', 'dob', 'height_cm', 'weight_kg', 'nationality', 'passing', 'defending', 'shooting',
+                 'dribbling', 'pace', 'overall', 'team_jersey_numbe']
 
-def do_sql(query):
+
+def do_sql(query, params=[]):
     with connection.cursor() as cursor:
-        cursor.execute(query)
+        cursor.execute(query, params)
         columns = [col[0] for col in cursor.description]
         return columns, [
             dict(zip(columns, row))
@@ -15,7 +17,24 @@ def do_sql(query):
 
 
 def view_players(request):
-    columns, players = do_sql("SELECT * FROM players;")
+    params = []
+    query = "SELECT * FROM players"
+    first_param = True
+    for field in player_fields:
+        param = request.GET.get(field)
+        print("field=", field, "param=", param)
+        if param is not None:
+            if first_param:
+                query += " WHERE"
+                first_param = False
+            else:
+                query += " AND"
+            query += " " + field + " = %s"
+            params.append(param)
+
+    query += ";"
+
+    columns, players = do_sql(query, params)
     for column in columns:
         print(column)
     context = {'players': players, 'columns': columns}
