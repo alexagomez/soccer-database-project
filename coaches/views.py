b8ed031f33212fa3da9f2cc0e16f6d81c01b5408
Goutham Thiagarajan <gst6yg@virginia.edu>
2020-04-25 22:43:35 -0400

    Added alt tables to coaches

--- coaches/views.py
+++ coaches/views.py
@@ -8,6 +8,12 @@ table_name = 'coaches'
 primary_key = 'full_name'
 table_columns = ['full_name','nationality','years_experience','num_championships','num_wins','num_losses','num_ties']
 
+alt_tables = {
+    'plays_or_coaches_for': {
+        'primary_key': 'long_name',
+        'table_columns': ['long_name', 'club_team', 'national_team']
+    }
+}
 
 def do_sql(query, params=[]):
     with connection.cursor() as cursor:
@@ -36,6 +42,13 @@ def get_fields_and_params(request_dict):
             fields.append(field + " = %s")
             params.append(param)
 
+    for table_info in alt_tables.values():
+        for field in table_info['table_columns']:
+            if field != primary_key:
+                param = request_dict.get(field)
+                if param is not None:
+                    field_to_param[field + " = %s"] = param
+
     return fields, params
 
 
@@ -43,14 +56,22 @@ def view_coaches(request):
     """
         Searches for all records based on query params. returns all records if no query params are present.
     """
-    query = "SELECT * FROM " + table_name
-    fields, params = get_fields_and_params(request.GET)
+    fields_and_params = get_fields_and_params(request.GET)
+    pk_val_query = "SELECT " + primary_key + " FROM " + table_name + ' NATURAL JOIN ' + ' NATURAL JOIN '.join(alt_tables.keys())
 
-    if len(fields) != 0:
-        query += " WHERE " + " AND ".join(fields)
+    if len(fields_and_params) != 0:
+        pk_val_query += " WHERE " + " AND ".join(fields_and_params.keys())
+    pk_val_query += ";"
+    pk, pk_vals = do_sql(pk_val_query, fields_and_params.values())
+
+    query = "SELECT * FROM " + table_name + ' NATURAL JOIN ' + ' NATURAL JOIN '.join(alt_tables.keys())
+    fields_and_params = get_fields_and_params(request.GET)
+
+    if len(fields_and_params) != 0:
+        query += " WHERE " + " AND ".join(fields_and_params.keys())
     query += ";"
+    columns, records = do_sql(query, fields_and_params.values())
 
-    columns, records = do_sql(query, params)
     context = {'records': records, 'columns': columns, 'params': request.GET}
     return render(request, table_name + '.html', context)
 
@@ -60,21 +81,30 @@ def edit_coach(request):
         Query parameter will just be the coach's name. We will look up original values to populate current view and then
         when user clicks submit send all of the values in the update statement. Name cannot be changed.
     """
+    fields_and_params = get_fields_and_params(request.GET)
+    pk_val_query = "SELECT " + primary_key + " FROM " + table_name + ' NATURAL JOIN ' + ' NATURAL JOIN '.join(
+        alt_tables.keys())
+
+    if len(fields_and_params) != 0:
+        pk_val_query += " WHERE " + " AND ".join(fields_and_params.keys())
+    pk_val_query += ";"
+    pk, pk_vals = do_sql(pk_val_query, fields_and_params.values())
+
     if request.method == 'GET':  # this is when the user clicks on a record's pk from the view template
         coach_name = request.GET.get(primary_key, None)
         if coach_name is None:
             context = {'record': {primary_key: 'None'}}
             return render(request, 'edit_coach.html', context)
 
-        query = "SELECT * FROM " + table_name
-        fields, params = get_fields_and_params(request.GET)
+        query = "SELECT * FROM " + table_name + ' NATURAL JOIN ' + ' NATURAL JOIN '.join(alt_tables.keys())
+        fields_and_params = get_fields_and_params(request.GET)
 
-        if len(fields) != 0:
-            query += " WHERE " + " AND ".join(fields)
+        if len(fields_and_params) != 0:
+            query += " WHERE " + " AND ".join(fields_and_params.keys())
         query += ";"
 
-        columns, record = do_sql(query, params)
-
+        columns, record = do_sql(query, fields_and_params.values())
+        
         context = {'record': record[0], 'columns': columns, 'params': request.GET}
         return render(request, 'edit_coach.html', context)
 
@@ -83,12 +113,27 @@ def edit_coach(request):
         old_pk_val = unquote(str(request.get_full_path()).split("=")[1])
         new_pk_val = request.POST.get(primary_key)
 
-        fields, params = get_fields_and_params(request.POST)
+        fields_and_params = get_fields_and_params(request.POST)
 
-        if len(fields) != 0:
-            query += " SET " + ", ".join(fields) + " WHERE " + primary_key + " = %s;"
+        if len(fields_and_params) != 0:
+            main_dict = {k: v for k, v in fields_and_params.items() if k.split(" ")[0] in table_columns}
+            query += " SET " + ", ".join(main_dict.keys()) + " WHERE " + \
+                     primary_key + " = %s;"
+            params = list(main_dict.values())
             params.append(old_pk_val)
-            if do_sql(query, params):
+            all_performed = do_sql(query, params)
+
+            for alt_table_name, table_info in alt_tables.items():
+                alt_dict = {k: v for k, v in fields_and_params.items() if k.split(" ")[0] in table_info['table_columns']}
+                query = "UPDATE " + alt_table_name
+                query += " SET " + ", ".join(alt_dict.keys()) \
+                         + " WHERE " + primary_key + " = %s;"
+                params = list(alt_dict.values())
+                params.append(old_pk_val)
+                if not do_sql(query, params):
+                    all_performed = False
+
+            if all_performed:
                 return redirect('/' + table_name + '/view?' + primary_key + '=' + new_pk_val)
 
         return redirect('/' + table_name + '/edit?' + primary_key + '=' + old_pk_val)
@@ -96,17 +141,29 @@ def edit_coach(request):
 
 def add_coach(request):
     if request.method == 'GET':
-        context = {'record': 'None', 'columns': table_columns}
+        full_columns = copy.deepcopy(table_columns)
+        for table_info in alt_tables.values():
+            full_columns += table_info['table_columns'][1:]
+        context = {'record': 'None', 'columns': full_columns}
         return render(request, 'add_coach.html', context)
 
     if request.method == 'POST':
         query = 'INSERT INTO ' + table_name + ' VALUES ('
 
-        fields, params = get_fields_and_params(request.POST)
-
-        placeholders = ["%s"] * len(params)
+        fields_and_params = get_fields_and_params(request.POST)
+        main_dict = {k: v for k, v in fields_and_params.items() if k.split(" ")[0] in table_columns}
+        placeholders = ["%s"] * len(main_dict)
         query += ", ".join(placeholders) + ");"
-        if do_sql(query, params):
+        all_performed = do_sql(query, main_dict.values())
+        for alt_table_name, table_info in alt_tables.items():
+            alt_dict = {k: v for k, v in fields_and_params.items() if k.split(" ")[0] in table_info['table_columns']}
+            query = 'INSERT INTO ' + alt_table_name + ' VALUES ('
+            placeholders = ["%s"] * len(alt_dict)
+            query += ", ".join(placeholders) + ");"
+            if not do_sql(query, alt_dict.values()):
+                all_performed = False
+
+        if all_performed:
             return redirect('/' + table_name + '/view?' + primary_key + '=' + request.POST.get(primary_key))
         else:
             return redirect('/' + table_name + '/add')
