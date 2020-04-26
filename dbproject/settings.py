a9f8b607200e25b87fff70958a38b495ff2e54f5
hank-weber <hpw3nr@virginia.edu>
2020-04-25 21:17:14 -0400

    remove club_teams standalone app

--- dbproject/settings.py
+++ dbproject/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'club_teams',
     'coaches',
     'players',
     'users',
--- dbproject/urls.py
+++ dbproject/urls.py
@@ -23,7 +23,6 @@ from users.views import login
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leagues/', include('leagues.urls')),
-    path('club_teams/', include('club_teams.urls')),
     path('favorite_teams/', include('favorite_teams.urls')),
     path('favorite_players/', include('favorite_players.urls')),
     path('coaches/', include('coaches.urls')),
