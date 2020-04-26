24b6310f481b480fc3a18d0874fe882348661374
hank-weber <hpw3nr@virginia.edu>
2020-04-26 15:15:12 -0400

    handle null values better in frontend

--- favorite_players/templatetags/favorite_players_extras.py
+++ favorite_players/templatetags/favorite_players_extras.py
@@ -5,6 +5,8 @@ register = template.Library()
 @register.simple_tag
 def dict_get(dct, key1):
     try:
+        if dct.get(key1) is None:
+            return ""
         return dct.get(key1, "")
     except AttributeError:
         return ""
